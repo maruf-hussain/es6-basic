909dbb290e11821a4fc53ea9899e0379526985e9
maruf-hussain <engmarufhussain@gmail.com>
2025-08-20 03:24:55 +0600

    spread

---
+++ arrowFunction.js
@@ -0,0 +1,14 @@
+const add = (a,b) =>{
+    return a + b;
+};
+const sum = add(50,20);
+console.log(sum);
+
+const mul = (a,b,c,d) => {
+    const e = a +b;
+    const f = c * d;
+    const g = e + f;
+    return g;
+};
+const total = mul(20,10,5,30);
+console.log(total)
---
+++ spreed.js
@@ -0,0 +1,5 @@
+const num = [20,4,34,33,12];
+const num2 = [...num];
+num2.push(59);
+console.log(num)
+console.log(num2)
